738e7ab539a7a2fd7bcba414fcf5f5751cb3a2cd
Ryan D. Najac <rdn2108@columbia.edu>
2023-12-18 19:08:31 -0500

    add math

--- galois/galois.cpp
+++ galois/galois.cpp
@@ -22,17 +22,17 @@
  * Provides a framework for performing arithmetic operations in a finite set of elements.
  */
 class GaloisField {
-  public:
+public:
     const unsigned int power;               ///< Power of the field (2^n).
     const unsigned int primitive;           ///< Primitive polynomial for the field.
     const unsigned int characteristic;      ///< Characteristic of the field (2^n).
     std::vector<unsigned long> powTable;    ///< Table for exponentiation.
     std::unordered_map<unsigned long, size_t> logTable; ///< Table for logarithms.
 
-void initializeTables() {
+    void initTables()
+    {
         // Resize powTable for range [0, 2^n - 1]
         powTable.resize(characteristic + 1);
-
         // Reserve space for logTable for range [1, 2^n - 1]
         logTable.reserve(characteristic);
 
@@ -45,15 +45,16 @@ void initializeTables() {
         // Ensuring the cyclic property
         powTable[characteristic] = 1;
     }
-  public:
+
+public:
     /**
      * @brief Constructor for GaloisField.
      * @param n The power of the field.
      */
     GaloisField(unsigned int n) : power(n), primitive(primes.at(n)), characteristic((1 << n) - 1)
-  {
-         initializeTables();
-  }
+    {
+        initTables();
+    }
 
     /**
      * @brief Default destructor for GaloisField.
@@ -63,21 +64,103 @@ void initializeTables() {
     /**
      * @brief Function to print members.
      */
-    void print() const {
-        std::cout << "\nPower = " << power << ", Primitive = " << primitive << ", Characteristic = " << characteristic << "\n";
+    void print() const
+    {
         int width = std::to_string(characteristic).length();
 
+        std::cout << "\nPower = " << power << ",\tPrimitive = " << primitive << ",\tCharacteristic = " << characteristic << "\n";
         std::cout << "Pow Table:       Log Table:\n";
-        std::cout << "pow(" << std::setw(width) << 0 << ") = " << std::setw(width) << powTable[0] << "\n";  // Separate line for powTable(0)
-
+        std::cout << "pow(" << std::setw(width) << 0 << ") = " << std::setw(width) << powTable[0] << "\n";
         for (size_t i = 1; i < powTable.size(); ++i) {
             std::cout << "pow(" << std::setw(width) << i << ") = " << std::setw(width) << powTable[i];
-            if (i < logTable.size()) {
+            if (i < logTable.size())
                 std::cout << "    " << "log(" << std::setw(width) << i << ") = " << std::setw(width) << logTable.at(i);
-            }
             std::cout << "\n";
         }
     }
 
+    /**
+     * @brief Multiply two elements in the Galois Field without using lookup tables.
+     * @param a First element.
+     * @param b Second element.
+     * @return Result of the multiplication.
+     */
+    unsigned int multNoLUT(unsigned int a, unsigned int b)
+    {
+        unsigned int result = 0;
+        while (b > 0) {
+            if (b & 1)
+                result ^= a;
+            a = multNoLUT(a, 2);
+            b >>= 1;
+        }
+        return result;
+    }
+
+    /**
+     * @brief Multiply two elements in the Galois Field using lookup tables.
+     * @param a First element.
+     * @param b Second element.
+     * @return Result of the multiplication.
+     */
+    unsigned int mult(unsigned int a, unsigned int b)
+    {
+        if (a == 0 || b == 0)
+            return 0;
+
+        return powTable[(logTable[a] + logTable[b]) % (characteristic - 1)];
+    }
+
+    /**
+     * @brief Divide two elements in the Galois Field.
+     * @param a Numerator.
+     * @param b Denominator (non-zero).
+     * @return Result of the division.
+     */
+    unsigned int div(unsigned int a, unsigned int b)
+    {
+        assert(b != 0);
+        if (a == 0)
+            return 0;
+        return powTable[(logTable[a] + (characteristic - 1) - logTable[b]) % (characteristic - 1)];
+    }
+
+    /**
+     * @brief Raise an element to a power in the Galois Field.
+     * @param x Base element.
+     * @param power Exponent.
+     * @return Result of the exponentiation.
+     */
+    unsigned int pow(unsigned int x, unsigned int power)
+    {
+        assert(x != 0 || power != 0);  // 0^0 is undefined
+        if (x == 0)
+            return 0;
+        if (power == 0)
+            return 1;
+        return powTable[(logTable[x] * power) % (characteristic - 1)];
+    }
+
+    /**
+     * @brief Compute the multiplicative inverse of an element in the Galois Field.
+     * @param x Element to invert (non-zero).
+     * @return Multiplicative inverse of x.
+     */
+    unsigned int inv(unsigned int x)
+    {
+        assert(x != 0);
+        return powTable[(characteristic - 1) - logTable[x]];
+    }
+
+    /**
+     * @brief Compute the square root of an element in the Galois Field.
+     * @param x Element for which to compute the square root (non-zero).
+     * @return Square root of x.
+     */
+    unsigned int sqrt(unsigned int x)
+    {
+        assert(x != 0);
+        return powTable[(logTable[x] + ((characteristic - 1) - 1)) / 2];
+    }
 };
 
--- galois/test_galois.cpp
+++ galois/test_galois.cpp
@@ -17,49 +17,32 @@ int main() {
 
     GaloisField gf(power);
 
-    // Print the tables
     gf.print();
-#if 0
+
     // Random number generation
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<unsigned int> distribution(1, gf.characteristic - 1); // Avoid 0 for division and inverse
 
-    // Generate random numbers
     unsigned int a = distribution(gen);
     unsigned int b = distribution(gen);
 
     std::cout << "\nRandom numbers: a = " << a << ", b = " << b << std::endl;
-
     // Test multiplication without lookup tables
     //unsigned int resultNoLUT = gf.multNoLUT(a, b);
     //std::cout << "\nMultiplication without LUT: " << a << " * " << b << " = " << resultNoLUT << std::endl;
-
     // Test multiplication with lookup tables
     unsigned int resultWithLUT = gf.mult(a, b);
     std::cout << "Multiplication with LUT: " << a << " * " << b << " = " << resultWithLUT << std::endl;
 
-    // Test regular multiplication
-    unsigned int resultRegularMul = a * b;
-    std::cout << "Regular Multiplication: " << resultRegularMul << std::endl;
-
-    std::cout << std::endl;
-
     // Test division
     unsigned int numerator = distribution(gen);
     unsigned int denominator = distribution(gen);
-
     std::cout << "\nRandom numbers: numerator = " << numerator << ", denominator = " << denominator << std::endl;
 
     // Test division in Galois field
     unsigned int resultDivGF = gf.div(numerator, denominator);
     std::cout << "Division in GF: " << numerator << " / " << denominator << " = " << resultDivGF << std::endl;
-
-    // Test regular division
-    unsigned int resultRegularDiv = numerator / denominator;
-    std::cout << "Regular Division: " << resultRegularDiv << std::endl;
-
-    std::cout << std::endl;
 
     // Test exponentiation
     unsigned int base = distribution(gen);
@@ -67,45 +50,30 @@ int main() {
 
     std::cout << "\nRandom numbers: base = " << base << ", exponent = " << exponent << std::endl;
 
-    // Test exponentiation in Galois field
     unsigned int resultPowGF = gf.pow(base, exponent);
     std::cout << "Exponentiation in GF: " << base << " ^ " << exponent << " = " << resultPowGF << std::endl;
 
-    // Test regular exponentiation
     unsigned int resultRegularPow = static_cast<unsigned int>(std::pow(base, exponent));
     std::cout << "Regular Exponentiation: " << resultRegularPow << std::endl;
-
     std::cout << std::endl;
 
     // Test inverse
     unsigned int elementToInvert = distribution(gen);
+    std::cout << "\nRandom number: " << elementToInvert << std::endl;
 
-    std::cout << "\nRandom number: elementToInvert = " << elementToInvert << std::endl;
-
-    // Test inverse in Galois field
     unsigned int resultInvGF = gf.inv(elementToInvert);
     std::cout << "Multiplicative Inverse in GF: 1 / " << elementToInvert << " = " << resultInvGF << std::endl;
 
-    // Test regular inverse
     unsigned int resultRegularInv = static_cast<unsigned int>(1.0 / elementToInvert);
     std::cout << "Regular Multiplicative Inverse: 1 / " << elementToInvert << " = " << resultRegularInv << std::endl;
 
-    std::cout << std::endl;
-
     // Test square root
     unsigned int elementForSqrt = distribution(gen);
-
-    std::cout << "\nRandom number: elementForSqrt = " << elementForSqrt << std::endl;
-
-    // Test square root in Galois field
+    std::cout << "\nRandom number: " << elementForSqrt << std::endl;
     unsigned int resultSqrtGF = gf.sqrt(elementForSqrt);
     std::cout << "Square Root in GF: " << resultSqrtGF << std::endl;
 
-    // Test regular square root
-    unsigned int resultRegularSqrt = static_cast<unsigned int>(std::sqrt(elementForSqrt));
-    std::cout << "Regular Square Root: " << resultRegularSqrt << std::endl;
-
-#endif
+    std::cout << std::endl;
     return 0;
 }
 
